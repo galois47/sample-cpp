b065a3e6314c7c5d810ce03404cf6a0ee9d2712e
Galois47 <andrewbrut47@gmail.com>
2026-03-01 14:53:41 -0700

    Improve dereferencing of values.

--- dynamic_library/usage/src/using_vector.cpp
+++ dynamic_library/usage/src/using_vector.cpp
@@ -3,18 +3,18 @@
 #include "vector4.h"
 
 int main() { 
-    Vector3 v3(1.0, 1.0, 1.0);
+    Vector3 v3(30, 31, 32);
     std::cout << "Using Vector 3 Class! \n";
     std::cout << "X Coordinate: " << v3.getXCoordinate() << "\n";
     std::cout << "Y Coordinate: " << v3.getYCoordinate() << "\n";
     std::cout << "Z Coordinate: " << v3.getZCoordinate() << std::endl;
 
-    Vector4 v4(2.0, 2.0, 2.0, 2.0);
+    Vector4 v4(40, 41, 42, 43);
     std::cout << "Using Vector 4 Class! \n";
     std::cout << "X Coordinate: " << v4.getXCoordinate() << "\n";
     std::cout << "Y Coordinate: " << v4.getYCoordinate() << "\n";
-    std::cout << "Z Coordinate: " << v4.getYCoordinate() << "\n";
-    std::cout << "U Coordinate: " << v4.getZCoordinate() << std::endl;
+    std::cout << "Z Coordinate: " << v4.getZCoordinate() << "\n";
+    std::cout << "U Coordinate: " << v4.getUCoordinate() << std::endl;
 
     return 0;
 }
