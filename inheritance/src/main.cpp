a23a8fa1693457005c0d01f9d4882cb377bcf724
Galois47 <andrewbrut47@gmail.com>
2026-03-01 19:11:40 -0700

    Formatting.

--- inheritance/src/main.cpp
+++ inheritance/src/main.cpp
@@ -6,21 +6,21 @@ int main() {
     //Using derived class method
     Car* carPtr = new Car("Ford", "Mustang");
     carPtr->displayDetails();
-    std::cout << "Car Ptr Memory Location: " <<carPtr << "\n";
-    std::cout << "-------------------------" << "\n";
+    std::cout << "Car Ptr Memory Location:     " <<carPtr << "\n";
+    std::cout << "-----------------------------" << "\n";
 
     //Using based class method
     Vehicle* vehiclePtr = new Vehicle("Honda");
     vehiclePtr->displayDetails();
     std::cout << "Vehicle Ptr Memory Location: " <<vehiclePtr << "\n";
-    std::cout << "-------------------------" << "\n";
+    std::cout << "-----------------------------" << "\n";
 
     //Vehicle from car details
     vehiclePtr = carPtr;
     vehiclePtr->displayDetails();
     std::cout << "Car Ptr Memory Location:     " <<carPtr << "\n";
     std::cout << "Vehicle Ptr Memory Location: " <<vehiclePtr << "\n";
-    std::cout << "-------------------------" << "\n";
+    std::cout << "-----------------------------" << std::endl;
 
     //Deleting heap allocated ptr
     delete vehiclePtr;
