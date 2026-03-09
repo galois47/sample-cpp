0d526bccbcc998aa53ae2de1493694c925ca273e
Galois47 <andrewbrut47@gmail.com>
2026-03-08 20:52:56 -0600

    Initial pass at processing request and deserializing json.

--- http_server/src/http_server.cpp
+++ http_server/src/http_server.cpp
@@ -8,7 +8,15 @@ using json = nlohmann::json;
 
 void hi_endpoint_callback(const httplib::Request &req, httplib::Response &res)
 {
-    std::cout << "Printing Request Body" << req.body << std::endl;
+    std::cout << "Printing Request Body " << req.body << std::endl;
+
+    //Deserializing request into json-like object
+    json ex2 = nlohmann::json::parse(req.body);
+    std::cout << "One: " << ex2["one"] << std::endl;
+    std::cout << "Two: " << ex2["two"] << std::endl;
+    std::cout << "Three: " << ex2["three"] << std::endl;
+
+    //Responding with same body in request
     res.set_content(req.body, "text/plain");
 };
 
@@ -16,18 +24,7 @@ int main()
 {
     httplib::Server svr;
 
-    json ex1 = nlohmann::json::parse(R"(
-    {
-        "pi": 3.141,
-        "happy": true
-    }
-    )");
-
-    std::cout << "Pi: " << ex1["pi"] << std::endl;
-    std::cout << "Happy: " << ex1["happy"] << std::endl;
-
     svr.Get("/hi", hi_endpoint_callback);
-
     svr.listen("0.0.0.0", 8080);
 
     return 0;
